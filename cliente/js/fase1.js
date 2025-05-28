309a8f65fff0646606889260a6b9daa7cea02218
Ederson Torresini <etorresini@ifsc.edu.br>
2025-05-28 00:05:22 -0300

    Primeira parte: sincronizar pássaros

--- cliente/js/fase1.js
+++ cliente/js/fase1.js
@@ -1,13 +1,11 @@
+/*global Phaser*/
+/*eslint no-undef: "error"*/
 export default class fase1 extends Phaser.Scene {
   constructor() {
     super("fase1");
     this.speed = 200;
     this.score = 0;
     this.tirosRestantes = 6;
-    this.passarosRestantes = 4;
-    this.maxPassaros = 4;
-    this.totalPassarosGerados = 0;
-    this.aguardandoNovaRodada = false;
   }
 
   preload() {
@@ -37,8 +35,6 @@ export default class fase1 extends Phaser.Scene {
     this.mira = this.physics.add
       .sprite(100, 100, "mira")
       .setCollideWorldBounds(true);
-    this.passaros = this.physics.add.group();
-    this.tempoParaNovoPassaro = 0;
 
     this.anims.create({
       key: "voar",
@@ -67,6 +63,22 @@ export default class fase1 extends Phaser.Scene {
       }),
       frameRate: 1,
     });
+
+    this.passaros = this.physics.add.group();
+    for (let i = 0; i < 15; i++) {
+      const backgroundY = 190;
+      const backgroundHeight = 380;
+      const topLimit = backgroundY - backgroundHeight / 2;
+      const bottomLimit = backgroundY + backgroundHeight / 2;
+
+      const y = Phaser.Math.Between(topLimit + 20, bottomLimit - 20);
+      const direcao = Phaser.Math.Between(0, 1) === 0 ? -1 : 1;
+      const x = direcao === 1 ? -50 : 850;
+
+      const passaro = this.passaros.create(x, y, "pomba-branca");
+      passaro.direcao = direcao;
+      passaro.anims.play("voar", true);
+    }
 
     if (this.game.jogadores.primeiro === this.game.socket.id) {
       this.game.remoteConnection = new RTCPeerConnection(this.game.iceServers);
@@ -112,8 +124,16 @@ export default class fase1 extends Phaser.Scene {
         this.game.remoteConnection.addIceCandidate(candidate);
       });
 
+      this.passaros.children.entries.forEach((passaro) => {
+        passaro.setVelocity(
+          Phaser.Math.Between(100, 150) * passaro.direcao,
+          Phaser.Math.Between(-80, 80),
+        );
+        passaro.setFlipX(passaro.direcao === -1);
+      });
+
       this.personagemLocal = this.physics.add.sprite(100, 100, "mira");
-      //this.personagemRemoto = this.add.sprite(100, 150, "mira")
+      this.personagemRemoto = this.add.sprite(100, 150, "mira");
     } else if (this.game.jogadores.segundo === this.game.socket.id) {
       this.game.localConnection = new RTCPeerConnection(this.game.iceServers);
       this.game.dadosJogo = this.game.localConnection.createDataChannel(
@@ -160,18 +180,36 @@ export default class fase1 extends Phaser.Scene {
       });
 
       this.personagemLocal = this.physics.add.sprite(100, 100, "mira");
-      //this.personagemRemoto = this.add.sprite(100, 150, "mira");
+      this.personagemRemoto = this.add.sprite(100, 150, "mira");
     }
 
     this.game.dadosJogo.onopen = () => {
       console.log("Conexão de dados aberta");
     };
+
     this.game.dadosJogo.onmessage = (event) => {
       const dados = JSON.parse(event.data);
-      if (dados.persongem) {
+
+      if (dados.personagem) {
         this.personagemRemoto.X = dados.personagem.x;
         this.personagemRemoto.Y = dados.personagem.y;
         this.personagemRemoto.setFrame(dados.personagem.frame);
+      }
+
+      if (dados.passaros) {
+        dados.passaros.forEach((passaro, i) => {
+          if (this.passaros.children.entries[i]) {
+            this.passaros.children.entries[i].x = passaro.x;
+            this.passaros.children.entries[i].y = passaro.y;
+            if (!passaro.visible) {
+              this.passaros.children.entries[i].setVisible(false);
+            }
+          }
+        });
+      }
+      
+      if (dados.passaroAtingido){
+        this.passaros.children.entries[dados.passaroAtingido].setVisible(false);
       }
     };
 
@@ -182,39 +220,16 @@ export default class fase1 extends Phaser.Scene {
       fontSize: "32px",
       fill: "#fff",
     });
+
     this.tirosText = this.add.text(16, 60, "Tiros: 6", {
       fontSize: "28px",
       fill: "#fff",
     });
+
     this.rodadaText = this.add
       .text(400, 300, "", { fontSize: "40px", fill: "#ffff00" })
       .setOrigin(0.5)
       .setDepth(1);
-
-    this.spawnPassaro = () => {
-      if (this.totalPassarosGerados >= this.maxPassaros) return;
-
-      const backgroundY = 190;
-      const backgroundHeight = 380;
-      const topLimit = backgroundY - backgroundHeight / 2;
-      const bottomLimit = backgroundY + backgroundHeight / 2;
-
-      const y = Phaser.Math.Between(topLimit + 20, bottomLimit - 20);
-      const direcao = Phaser.Math.Between(0, 1) === 0 ? -1 : 1;
-      const x = direcao === 1 ? -50 : 850;
-
-      const passaro = this.passaros.create(x, y, "pomba-branca");
-      passaro.setVelocity(
-        Phaser.Math.Between(100, 150) * direcao,
-        Phaser.Math.Between(-80, 80),
-      );
-      passaro.direcao = direcao;
-      passaro.setFlipX(direcao === -1);
-      passaro.acertado = false;
-
-      passaro.anims.play("voar", true);
-      this.totalPassarosGerados++;
-    };
 
     this.input.gamepad.once("connected", (pad) => {
       this.gamepad = pad;
@@ -227,102 +242,7 @@ export default class fase1 extends Phaser.Scene {
     });
   }
 
-  update(time, delta) {
-    if (
-      this.input.gamepad &&
-      this.input.gamepad.total > 0 &&
-      !this.aguardandoNovaRodada
-    ) {
-      const pad = this.input.gamepad.getPad(0);
-      const axisH = pad.axes[0].getValue();
-      const axisV = pad.axes[1].getValue();
-      const botaoTiro = pad.buttons[2].pressed;
-
-      this.mira.setVelocity(this.speed * axisH, this.speed * axisV);
-
-      this.passaros.getChildren().forEach((passaro) => {
-        const colidiu = Phaser.Geom.Intersects.RectangleToRectangle(
-          this.mira.getBounds(),
-          passaro.getBounds(),
-        );
-
-        if (
-          colidiu &&
-          botaoTiro &&
-          !this.ultimoTiro &&
-          !passaro.acertado &&
-          this.tirosRestantes > 0
-        ) {
-          passaro.acertado = true;
-          this.fire.play();
-          passaro.setVelocity(0, 0);
-          passaro.anims.play("impacto");
-
-          passaro.once("animationcomplete", () => {
-            passaro.setVelocity(0, 100);
-            passaro.anims.play("queda");
-
-            passaro.once("animationcomplete", () => {
-              passaro.destroy();
-            });
-          });
-
-          this.score += 10;
-          this.passarosRestantes--;
-          this.tirosRestantes--;
-          this.registry.set("score", this.score);
-          this.scoreText.setText("Pontuação: " + this.score);
-          this.tirosText.setText("Tiros: " + this.tirosRestantes);
-          this.ultimoTiro = true;
-        }
-      });
-
-      if (botaoTiro && !this.ultimoTiro && this.tirosRestantes > 0) {
-        this.fire.play();
-        this.tirosRestantes--;
-        this.tirosText.setText("Tiros: " + this.tirosRestantes);
-        this.ultimoTiro = true;
-      }
-
-      if (!botaoTiro) this.ultimoTiro = false;
-    }
-
-    if (!this.aguardandoNovaRodada && this.passarosRestantes > 0) {
-      this.tempoParaNovoPassaro += delta;
-      if (this.tempoParaNovoPassaro > 1500) {
-        this.tempoParaNovoPassaro = 0;
-        if (this.passaros.countActive(true) < this.maxPassaros) {
-          this.spawnPassaro();
-        }
-      }
-    }
-
-    this.passaros.getChildren().forEach((passaro) => {
-      if (
-        passaro.x < -60 ||
-        passaro.x > 860 ||
-        passaro.y < -60 ||
-        passaro.y > 600
-      ) {
-        if (!passaro.acertado) {
-          this.passarosRestantes--;
-        }
-        passaro.destroy();
-      }
-    });
-
-    if (
-      this.passarosRestantes === 0 &&
-      this.tirosRestantes >= 0 &&
-      !this.aguardandoNovaRodada
-    ) {
-      this.aguardandoNovaRodada = true;
-      this.rodadaText.setText("Fase Completa!");
-      this.time.delayedCall(2000, () => {
-        this.irParaFase2();
-      });
-    }
-
+  update() {
     try {
       if (this.game.dadosJogo.readyState === "open") {
         if (this.personagemLocal) {
@@ -336,14 +256,55 @@ export default class fase1 extends Phaser.Scene {
             }),
           );
         }
+
+        if (
+          this.passaros &&
+          this.game.jogadores.primeiro === this.game.socket.id
+        ) {
+          this.game.dadosJogo.send(
+            JSON.stringify({
+              passaros: this.passaros.children.entries.map((passaro) =>
+                ((passaro) => ({
+                  x: passaro.x,
+                  y: passaro.y,
+                  visible: passaro.visible,
+                }))(passaro),
+              ),
+            }),
+          );
+        }
       }
     } catch (error) {
-      console.error(error);
+      // console.error(error);
     }
-  }
 
-  irParaFase2() {
-    this.scene.stop("fase1");
-    this.scene.start("fase2");
+    if (
+      this.input.gamepad &&
+      this.input.gamepad.total > 0 &&
+      !this.aguardandoNovaRodada
+    ) {
+      const pad = this.input.gamepad.getPad(0);
+      const axisH = pad.axes[0].getValue();
+      const axisV = pad.axes[1].getValue();
+      const botaoTiro = pad.buttons[2].pressed;
+
+      this.mira.setVelocity(this.speed * axisH, this.speed * axisV);
+
+      this.passaros.getChildren().forEach((passaro, i) => {
+        const colidiu = Phaser.Geom.Intersects.RectangleToRectangle(
+          this.mira.getBounds(),
+          passaro.getBounds(),
+        );
+
+        if (colidiu && botaoTiro) {
+          passaro.setVisible(false);
+          this.game.dadosJogo.send(
+            JSON.stringify({
+              passaroAtingido: i,
+            }),
+          );
+        }
+      });
+    }
   }
 }
