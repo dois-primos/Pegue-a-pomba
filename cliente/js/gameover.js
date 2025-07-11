90d8db6edb90407b2caf9bccec8b2eedd0b06702
Ederson Torresini <etorresini@ifsc.edu.br>
2025-07-10 21:15:12 -0300

    Adicionar gameover

--- cliente/js/gameover.js
+++ cliente/js/gameover.js
@@ -5,8 +5,30 @@ export default class gameover extends Phaser.Scene {
     super("gameover");
   }
 
-  init(data) {
+  init() {
     this.game.cenaAtual = "gameover";
-    this.score = data.score || 0;
+  }
+
+  preload() {
+    this.load.image("gameover", "assets/gameover.png"); // Fundo para o game over
+  }
+
+  create() {
+    this.add
+      .image(400, 225, "gameover")
+      .setInteractive()
+      .on("pointerdown", () => {
+        window.location.reload();
+      });
+
+    this.input.gamepad.on("down", (pad) => {
+      if (pad.buttons[9].pressed) {
+        window.location.reload();
+      }
+    });
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 10000);
   }
 }
